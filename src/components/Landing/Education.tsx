ed394d78b587a18784af93bcf8ce9d67b2fc561e
Inez Yoon <inez3cs@gmail.com>
2025-07-25 21:11:46 -0700

    Fix relative path to absolute aliases

--- src/components/Landing/Education.tsx
+++ src/components/Landing/Education.tsx
@@ -1,5 +1,5 @@
 import useScrollFadeIn from "./useScrollFadeIn";
-import CardCarousel from "../Reusables/CardCarousel";
+import CardCarousel from "@/components/Reusables/CardCarousel";
 
 const Education: React.FC = () => {
   const ref = useScrollFadeIn();
